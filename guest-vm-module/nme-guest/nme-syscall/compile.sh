1843113c60d57a8b8d44176db62336be0b4c7658
jqhong <hjqgzz@gmail.com>
2021-08-04 18:04:19 +0800

    Delete guest-vm-module directory

--- guest-vm-module/nme-guest/nme-syscall/compile.sh
+++
@@ -1,2 +0,0 @@
-gcc -c -Wall -Werror -fpic foo.c 
-gcc -shared -o nme.so foo.o
--- guest-vm-module/nme-guest/target/run.sh
+++
@@ -1,6 +0,0 @@
-cd ~/Documents/test_ker/fsgsbase_cr3/
-## ./install.sh
-sudo rmmod enable_rdfsbase
-sudo insmod enable_rdfsbase.ko
-cd ~/Documents/nme-guest/target/
-./nme_target
